dc7d666c2847545bed163a19f8c511fcad97a454
sledge79 <tonywalsh1511@gmail.com>
2023-07-20 20:42:18 +0000

    Build complete
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include "mbed.h"
 
+
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 
-const int LED1_TOGGLE_INTERVAL_S = 2000;
+
+const int LED1_TOGGLE_INTERVAL_MS = 2000;
 const int LED2_TOGGLE_INTERVAL_MS = 5000;
 
+
+void toggleLED1() {
+    static bool state = false;
+    led1 = state;
+    state = !state;
+}
+
+
+void toggleLED2() {
+    static bool state = false;
+    led2 = state;
+    state = !state;
+}
+
+int main() {
+   
+    Ticker ticker1;
+    Ticker ticker2;
+
+     ticker1.attach(toggleLED1, std::chrono::milliseconds(LED1_TOGGLE_INTERVAL_MS));
+    ticker2.attach(toggleLED2, std::chrono::milliseconds(LED2_TOGGLE_INTERVAL_MS));
+
+    
+    while (1) {
+      
+    }
+}
