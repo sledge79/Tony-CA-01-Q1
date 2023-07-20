84e1cdea79bddebec07ff928cf92858c69cc2d8a
sledge79 <tonywalsh1511@gmail.com>
2023-07-20 20:32:45 +0000

    first commit
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
 
-    }
-}
+const int LED1_TOGGLE_INTERVAL_S = 2000;
+const int LED2_TOGGLE_INTERVAL_MS = 5000;
 
